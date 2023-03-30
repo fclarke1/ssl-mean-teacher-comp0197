e703b633eff576561ca936aade44ae9e3cfdd132
Zythume <t.du.crest@hotmail.fr>
2023-03-31 00:40:04 +0100

    Print results

--- Training Latest/print_results.py
+++ Training Latest/print_results.py
@@ -6,7 +6,7 @@ import numpy as np
 import model_UNet
 import matplotlib.pyplot as plt
 from data_augmentation import augmentation
-from torchvision.utils import make_grid
+from data_into_loaders import get_data
 import torchvision.transforms.functional as F
 from PIL import Image, ImageDraw, ImageFont
 
@@ -14,6 +14,8 @@ def print_losses(name):
     '''Print the losses of the model and save the plot as a png file'''
     filelist = os.listdir()
     txtfiles = [file for file in filelist if file.endswith('loss')]
+    if len(txtfiles) == 0:
+        return
     #read losses in txt files
     df = join_txt(txtfiles)
     df = df.set_index(pd.Index(range(1, len(df)+1)))
@@ -23,11 +25,24 @@ def print_metrics(name):
     '''Print the metrics of the model and save the plot as a png file'''
     filelist = os.listdir()
     txtfiles = [file for file in filelist if (file.endswith('IOU') or file.endswith('acc'))]
+    if len(txtfiles) == 0:
+        return
     #read metrics in txt files
     df = join_txt(txtfiles)
     df = df.set_index(pd.Index(list(5 * np.arange(0, len(df)) + 1)))
     #plot and save dataframe
     save_plot(df, name, 'Epoch', 'Accuracy', 'Accuracy and IOU vs Epochs', dash = True)
+def print_weights(name):
+    '''Print the weights of the model and save the plot as a png file'''
+    filelist = os.listdir()
+    txtfiles = [file for file in filelist if file.endswith('weights')]
+    if len(txtfiles) == 0:
+        return
+    #read weights in txt files
+    df = join_txt(txtfiles)
+    df = df.set_index(pd.Index(range(1, len(df)+1)))
+    #plot and save dataframe
+    save_plot(df, name, 'Epoch', 'Weights', 'Weights vs Epochs')
 def save_plot(df, name, x_label, y_label, title, dash = False):
     '''Save the plot of the dataframe as a png file'''
     for i in range(len(df.columns)):
@@ -35,11 +50,12 @@ def save_plot(df, name, x_label, y_label, title, dash = False):
             plt.plot(df.iloc[:,i], linestyle = '--')
         else:
             plt.plot(df.iloc[:,i])
-    plt.legend(df.columns)
+    plt.legend(df.columns, loc = 1)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.title(title)
     plt.savefig(name + '.png')
+    plt.clf()
 def join_txt(txtfiles):
     '''Join the txt files in a dataframe'''
     df = pd.read_csv(txtfiles[0], sep = ' ', header = None, names = [txtfiles[0]])
@@ -53,12 +69,12 @@ def print_image_and_mask(image,label, model=0, depth = 3):
     with torch.no_grad():
         pred = network(image.unsqueeze(0))
     image_list = [image, augmentation(image)]
-    pred_list = [label, pred]
+    pred_list = [label.unsqueeze(1), pred]
     print_(image_list, ['Original', 'Augmented'], 'pictures.png')
     print_(pred_list, ['Label', 'Prediction'], 'mask.png')
 def print_(images, labels, file_name):
     nb_image = len(images)
-    images_size = images[0].shape[1]
+    images_size = images[0].shape[-1]
     marge = 4
     text_box = 25
     image_comb = Image.new('RGB',(marge * (2 * nb_image) + nb_image * images_size , marge * 2 + images_size + text_box), color = 'white')
@@ -78,8 +94,17 @@ def show(imgs):
         axs[0, i].imshow(np.asarray(img))
         axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
 if __name__ == '__main__':
-    image = torch.rand(3,64,64)
-    label = torch.rand(1,64,64)
+    supervised_pct = 0.25
+    val_pct, test_pct = 0.2, 0.1
+    batch_size = 32
+    img_resize = 64
+    mixed_train_loader, val_loader, test_loader = get_data(supervised_pct,1 - supervised_pct, val_pct, test_pct, batch_size=batch_size, img_resize=img_resize)
+    itertest = iter(test_loader)
+    images, labels = next(itertest)
+    image = images[0]
+    label = labels[0]
+    # image = torch.rand(3,64,64)
+    # label = torch.rand(1,64,64)
     files_list=os.listdir()
     for file in files_list:
         if file.find('.') == -1 and file != 'data' and file != '__pycache__':
@@ -87,12 +112,13 @@ if __name__ == '__main__':
             models_list = os.listdir()
             for model in models_list:
                 os.chdir(model)
+                print_weights('weights')
                 print_metrics('Accuracy and IOU')
                 print_losses('losses')
                 list_of_files = os.listdir()
                 weights_files = []
                 for file in list_of_files:
-                    if file.startswith(f'student_epoch'):
+                    if file.endswith('.pt'):
                         weights_files.append(file)
                 if weights_files != []:
                     weights_files = sorted(weights_files, key=lambda s: int(re.search(r'\d+', s).group()))
