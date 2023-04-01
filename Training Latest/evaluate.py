4998d594d85638c65da4a446be83eed496ee1c2b
Zythume <t.du.crest@hotmail.fr>
2023-04-01 02:08:15 +0100

    loss, accuracy and IOU printed

--- Training Latest/evaluate.py
+++ Training Latest/evaluate.py
@@ -1,11 +1,14 @@
 import os
 import torch
+import numpy as np
+import matplotlib.pyplot as plt
 from data_into_loaders import get_data
 from utils import evaluate_model
 from model_UNet import UNet
+from print_results import join_txt
 
 
-def print_Table(dictionary, remove = None):
+def print_Table(dictionary, remove = [], percent = True):
     '''Print the dictionary as a table'''
     #remove the keys that we don not want to print
     if remove:
@@ -28,11 +31,15 @@ def print_Table(dictionary, remove = None):
     print('-' *sum(col_size))
     for i in range(len(dictionary[item])):
         for j in range(len(dictionary.keys()) - 1):
-            if type(dictionary[list(dictionary.keys())[j]][i]) != str:
+            if list(dictionary.keys())[j] == 'Epoch' or percent == False:
+                print(f'{dictionary[list(dictionary.keys())[j]][i]}', end = ' ' * (col_size[j] - len(f'{dictionary[list(dictionary.keys())[j]][i]}')) + '|')
+            elif type(dictionary[list(dictionary.keys())[j]][i]) != str and percent:
                 print(f'{dictionary[list(dictionary.keys())[j]][i]:.2%}', end = ' ' * (col_size[j] - len(f'{dictionary[list(dictionary.keys())[j]][i]:.2%}')) + '|')
             else:
                 print(dictionary[list(dictionary.keys())[j]][i], end = ' ' * (col_size[j] - len(dictionary[list(dictionary.keys())[j]][i])) + '|')
-        if type(dictionary[list(dictionary.keys())[j]][i]) != str:
+        if list(dictionary.keys())[j + 1] == 'Epoch':
+                print(f'{dictionary[list(dictionary.keys())[j + 1]][i]}')
+        elif type(dictionary[list(dictionary.keys())[j + 1]][i]) != str:
             print(f'{dictionary[list(dictionary.keys())[j + 1]][i]:.2%}')
         else:
             print(dictionary[list(dictionary.keys())[j + 1]][i])
@@ -71,9 +78,46 @@ def evaluate():
                         break
                 os.chdir('..')
             os.chdir('..')
-    # print(dictionary)
+    barWidth = 0.25
+    fig = plt.subplots()
+    br1 = np.arange(len(dictionary['Case']))
+    br2 = [x + barWidth for x in br1]
+    plt.bar(br1, list(map(lambda x: x * 100, dictionary['Accuracy'])), width = barWidth)
+    plt.bar(br2, list(map(lambda x: x * 100, dictionary['IOU'])), width = barWidth)
+    plt.xlabel('Models')
+    plt.ylim(0,100)
+    plt.ylabel('Percentage(%)')
+    plt.xticks([r + barWidth for r in range(len(dictionary['Case']))], dictionary['Case'])
+    plt.legend(['Accuracy', 'IOU'])
+    plt.show()
     print_Table(dictionary,['Model'])
-                
-
+def metric_per_epoch():
+    files_list=os.listdir()
+    for file in files_list:
+        if file.find('.') == -1 and file != 'data' and file != '__pycache__':
+            os.chdir(file)
+            models_list = os.listdir()
+            for model in models_list:
+                os.chdir(model)
+                filelist = os.listdir()
+                metricstxtfiles = [file for file in filelist if (file.endswith('IOU') or file.endswith('acc') or file.endswith('accuracy'))]
+                if len(metricstxtfiles) == 0:
+                    return
+                metricsdf = join_txt(metricstxtfiles)
+                metricsdf['Epoch'] = list(map(lambda x: (x + 1) * 5, list(metricsdf.index)))
+                print('Recaptilation of the metrics for the model: ', model)
+                print_Table(metricsdf)
+                losstxtfiles = [file for file in filelist if file.endswith('loss')]
+                if len(losstxtfiles) == 0:
+                    return
+                lossdf = join_txt(losstxtfiles)
+                lossdf = lossdf.round(2)
+                lossdf['Epoch'] = list(map(lambda x: (x + 1), list(lossdf.index)))
+                print_Table(lossdf, percent = False)
+                os.chdir('..')
+            os.chdir('..')
+    #read metrics in txt files
+    
 if __name__ == '__main__':
+    metric_per_epoch()
     evaluate()
